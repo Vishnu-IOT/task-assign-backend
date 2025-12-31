1ee618da5c459b3b5d6e6db593e8634d628cef8c
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-12-31 14:07:17 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,6 @@ const { loginUser, taskView, taskUpdate } = require("../controllers/employee");
 const { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation } = require("../controllers/admin");
 const { verifyFirebaseToken } = require("../helpers/middleware");
 const { crt } = require("../models/sqlfile");
-const { findUserMDB, registerUserMDB } = require("../controllers/mdbadmin");
 const router = express.Router();
 
 router.post("/register", verifyFirebaseToken, registerUser)
@@ -15,26 +14,8 @@ router.post("/details", empDetails)
 router.post("/assigntask", taskTable)
 router.post("/excel", excelDetails)
 
-router.post("/mdbregister", registerUserMDB)
-router.post("/mdbfind", findUserMDB)
-
 router.get("/userscreate",tableUserCreation);
 router.get("/taskscreate",tableTaskCreation);
-
-router.post("/usersfire", async (req, res) => {
-    const { uid, email, emailVerified } = req.body;
-
-    await crt.promise().query(
-        `INSERT INTO usersfirebase (firebase_uid, email, email_verified)
-     VALUES (?, ?, ?)
-     ON DUPLICATE KEY UPDATE email_verified = ?`,
-        [uid, email, emailVerified, emailVerified]
-    );
-
-    res.status(200).send("User stored successfully");
-});
-
-
 
 
 module.exports = router;
