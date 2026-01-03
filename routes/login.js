b99495819d0103c041431e179f4e07341316b0bd
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2026-01-03 10:25:14 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { loginUser, taskView, taskUpdate } = require("../controllers/employee");
-const { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation } = require("../controllers/admin");
+const { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation, count } = require("../controllers/admin");
 const { verifyFirebaseToken } = require("../helpers/middleware");
 const { crt } = require("../models/sqlfile");
 const router = express.Router();
@@ -16,6 +16,8 @@ router.post("/excel", excelDetails)
 
 router.get("/userscreate",tableUserCreation);
 router.get("/taskscreate",tableTaskCreation);
+router.get("/counts",count);
 
 
 module.exports = router;
+
