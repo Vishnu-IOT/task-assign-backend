ea143d850b3d9249822ce8fd6a72a655f3640066
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2026-01-06 16:56:13 +0530

    Update employee.js

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,71 +2,51 @@ const { checkUser, getTask, updateTask } = require("../models/sqlfile");
 const bcrypt = require("bcrypt");
 
 async function loginUser(req, res) {
-
-    const datalog = req.user;
-    // const salt = 10;
-    // const pass = await bcrypt.hash(datalog.password, salt);
-    // console.log(pass);
-    console.log(datalog);
-    checkUser(datalog, (err, result) => {
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
-        console.log(result);
-        // bcrypt.compare(datalog.password, result[0].password, (err, data) => {
-            // console.log(data);
-            if (err) {
-                console.log(err);
-            }
-            if (result.length > 0) {
-                console.log(result);
-                return res.status(200).send({ success: true, datas: result });
-            } else {
-                return res.status(402).send({ success: false });
-            }
-        // })
-    });
-}
-
-function taskView(req, res) {
-    const datas = req.body;
-    console.log(req.body.due_date);
-    getTask(datas, (err, result) => {
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
-        console.log(result);
-        if (err) {
-            console.log(err);
-        }
+    const datas = req.user;
+    try {
+        const [result] = await checkUser(datas);
         if (result.length > 0) {
             console.log(result);
             return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(402).send({ success: false });
         }
-    })
-
+        return res.status(402).send({ success: false, datas: "No User Founded!" });
+    }
+    catch (err) {
+        return res.status(402).send({ success: false });
+    }
 }
 
-function taskUpdate(req, res) {
+async function taskView(req, res) {
     const datas = req.body;
-    updateTask(datas, (err, result) => {
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
-        console.log(result);
-        if (err) {
-            console.log(err);
-        }
-        if (result.affectedRows > 0) {
-            console.log(result);
+    try {
+        const [result] = await getTask(datas);
+        if (result.length > 0) {
             return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(402).send({ success: false });
         }
-    })
+        return res.status(400).send({ success: false, datas: "No Task Found!" });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
+}
+
+async function taskUpdate(req, res) {
+    const datas = req.body;
+    if (!datas.remarks) {
+        return res.status(402).send({ success: false, datas: "Remarks Cannot be Empty!" });
+    }
+    try {
+        const [result] = await updateTask(datas);
+        if (result.affectedRows > 0) {
+            return res.status(200).send({ success: true, datas: "Task Updated Successfully!" });
+        }
+        return res.status(400).send({ success: false, datas: "Error in Updating Task!" });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
 }
 
 
 module.exports = { loginUser, taskView, taskUpdate };
+
