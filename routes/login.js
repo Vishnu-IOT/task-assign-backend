24416617a5ba375afcfcacdf91df3698f4f0b434
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2026-01-06 18:17:06 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { loginUser, taskView, taskUpdate } = require("../controllers/employee");
-const { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation, count, dupilcateUser } = require("../controllers/admin");
+const { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation, count, dupilcateUser,addAdmin } = require("../controllers/admin");
 const { verifyFirebaseToken } = require("../helpers/middleware");
 const { crt } = require("../models/sqlfile");
 const router = express.Router();
@@ -18,9 +18,11 @@ router.post("/duplicate", dupilcateUser)
 router.get("/userscreate",tableUserCreation);
 router.get("/taskscreate",tableTaskCreation);
 router.get("/counts",count);
+router.get("/admin", addAdmin);
 
 
 module.exports = router;
+
 
 
 
