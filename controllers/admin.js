68e729e23b9d8857067e309f20dc45350fe9d948
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2026-01-06 17:03:44 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,25 +1,23 @@
 const { assignTask, employeeDetails, completedTask, createEmployee, empidCreation, dupilcateEntry, excelsheet, createUserTable, createTaskTable, employeeCount } = require("../models/sqlfile");
 const bcrypt = require("bcrypt");
 
-function taskAssign(req, res) {
+async function taskAssign(req, res) {
     const task = req.body;
-    console.log(task.title);
-    assignTask(task, (err, result) => {
-        if (err) {
-
-            return res.status(500).send({ success: false, message: "Database error" });
+    if (!task.assigned_to) {
+        return res.status(400).send({ success: false, datas: "Assigned To is required! Cannot be Empty!" });
+    }
+    try {
+        const [rows] = await assignTask(task);
+        console.log(rows.affectedRows);
+        if (rows.affectedRows > 0) {
+            console.log(rows);
+            return res.status(200).send({ success: true, datas: "Task Created Successfully!" });
         }
-        console.log(result);
-        if (err) {
-            console.log(err);
-        }
-        if (result.affectedRows > 0) {
-            console.log(result);
-            return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(400).send({ success: false });
-        }
-    })
+        return res.status(400).send({ success: "false" });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
 }
 
 async function empDetails(req, res) {
@@ -41,83 +39,67 @@ async function empDetails(req, res) {
         }
 }
 
-function taskTable(req, res) {
+async function taskTable(req, res) {
     const names = req.body;
-    console.log(names);
-
-    completedTask(names, (err, result) => {
-        if (err) {
-
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
-        console.log(result);
-        if (err) {
-            console.log(err);
-        }
+    try {
+        const [result] = await completedTask(names);
         if (result.length > 0) {
-            console.log(result);
             return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(400).send({ success: false });
         }
-    })
+        return res.status(400).send({ success: false, datas: "No Task Founded on Provided Date!" });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
 }
 
-function registerUser(req, res) {
+async function registerUser(req, res) {
     const names = req.body;
-    console.log(names);
-    createEmployee(names, (err, result) => {
-        console.log(result);
-        console.log(err);
-        console.log("entering");
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database Error" });
-        }
-        console.log(result);
-        empidCreation(result);
+    try {
+        const [result] = await createEmployee(names);
+        await empidCreation(result);
         if (result.affectedRows > 0) {
-            console.log(result);
-            return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(402).send({ success: false, datas: "Email or Phone Number Already Exists" });
+            return res.status(201).send({ success: true, datas: "User Created Successfully!" });
         }
-    })
+        return res.status(400).send({ success: false });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false, datas: "Email or Phone Number Already Exists" });
+    }
 }
 
-function dupilcateUser(req, res) {
+async function dupilcateUser(req, res) {
     const data = req.body;
-    dupilcateEntry(data, (err, result) => {
-        console.log(result);
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database Error" });
-        }
+    if (!data.email || !data.phonenumber) {
+        return res.status(500).send({ success: false, message: "Email/Phone Number is Empty!" });
+    }
+    try {
+        const [result] = await dupilcateEntry(data);
         if (result.length === 0) {
             return res.status(200).send({ success: true, datas: "No Data Found" });
         }
-        else {
-            return res.status(400).send({ success: false, datas: "Email or Phone Number Already Exists" });
-        }
-    })
+        return res.status(400).send({ success: false, datas: "Email or Phone Number Already Exists" });
+    }
+    catch (err) {
+        return res.status(500).send({ success: false });
+    }
 }
 
-function excelDetails(req, res) {
+async function excelDetails(req, res) {
     const dept = req.body;
-    console.log(dept);
-    excelsheet(dept, (err, result) => {
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
-        console.log(result);
-        if (err) {
-            console.log(err);
-        }
+    if (!dept) {
+        return res.status(402).send({ success: false, datas: "Department Cannot be Empty!" });
+    }
+    try {
+        const [result] = await excelsheet(dept);
         if (result.length > 0) {
-            console.log(result);
             return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(400).send({ success: false });
         }
-    })
+        return res.status(400).send({ success: false, datas: "No data Found on that Department" });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
 }
 
 function tableUserCreation(req, res) {
@@ -140,20 +122,21 @@ function tableTaskCreation(req, res) {
     }
 }
 
-function count(req, res) {
-    employeeCount((err, result) => {
-        if (err) {
-            return res.status(500).send({ success: false, message: "Database error" });
-        }
+async function count(req, res) {
+     try {
+        const [result] = await employeeCount();
         if (result.length > 0) {
             return res.status(200).send({ success: true, datas: result });
-        } else {
-            return res.status(402).send({ success: false });
         }
-    });
+        return res.status(400).send({ success: false });
+    }
+    catch (err) {
+        return res.status(400).send({ success: false });
+    }
 }
 
 module.exports = { taskAssign, empDetails, taskTable, registerUser, excelDetails, tableUserCreation, tableTaskCreation, count, dupilcateUser };
+
 
 
 
